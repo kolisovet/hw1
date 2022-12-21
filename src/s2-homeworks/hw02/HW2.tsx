d62f80641d3d6fc0d004d884b5d4f81d29524186
kolisovet <kolisovet@gmail.com>
2022-12-21 11:04:45 +0700

    3 nucom

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -68,6 +68,7 @@ function HW2() {
     return (
         <div id={'hw2'}>
             <div className={s2.hwTitle}>Homework #2</div>
+            <hr className={s2.line}/>
             <div className={s2.hw}>
                 <Affairs
                     data={filteredAffairs}
