66618453f3d0985f45f8f902f0d97b1b8565fa8c
kolisovet <kolisovet@gmail.com>
2023-03-06 18:12:40 +0700

    8

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -50,15 +50,17 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      name={o.name}
+                      name={name + '-' + o.id}
                       value={o.id}
                       checked={o.id === value}
+
                       // name, checked, value делают студенты
                       // http://htmlbook.ru/html/input/name
                   //checked={o.id === ...может попробовать значение которое тянем из HW7?}
 
                       onChange={onChangeCallback}
                       {...restProps}
+
                   />
                   <span
                       id={id + '-span-' + o.id}
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,33 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            // sort() создаёт новый массив? или нужно в ручную?...
+            if (action.payload === 'up')
+                return state.sort(function (a, b) {
+                    let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
+                    if (nameA < nameB) //сортируем строки по возрастанию
+                        return -1
+                    if (nameA > nameB)
+                        return 1
+                    return 0 // Никакой сортировки
+                }) // need to fix
+            else return state.sort(function (a,b){
+                let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
+                if (nameA < nameB) //сортируем строки по убыванию
+                    return 1
+                if (nameA > nameB)
+                    return -1
+                return 0 // Никакой сортировки
+            })
         }
-        case 'check': {
 
-            return state // need to fix
+
+        case 'check': {
+            // filter() создаёт новый массив? или нужно в ручную?...
+            return state.filter(el=> el.age >= action.payload) // need to fix
         }
         default:
             return state
