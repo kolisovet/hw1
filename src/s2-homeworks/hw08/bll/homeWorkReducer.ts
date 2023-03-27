f371ce04bc29d0866e1729fc84a364aa4a12befd
kolisovet <kolisovet@gmail.com>
2023-03-27 20:02:00 +0700

    10

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,29 +7,18 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            // sort() создаёт новый массив? или нужно в ручную?...
-            if (action.payload === 'up')
-                return state.sort(function (a, b) {
-                    let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
-                    if (nameA < nameB) //сортируем строки по возрастанию
-                        return -1
-                    if (nameA > nameB)
-                        return 1
-                    return 0 // Никакой сортировки
-                }) // need to fix
-            else return state.sort(function (a,b){
-                let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
-                if (nameA < nameB) //сортируем строки по убыванию
-                    return 1
-                if (nameA > nameB)
-                    return -1
-                return 0 // Никакой сортировки
+            // sort() создаёт новый массив? или нужно в ручную?
+             const copyState =[...state]
+            const newState = copyState.sort((a,b)=>{
+             if(a.name>b.name) return 1
+             else if(a.name <b.name)return -1
+             else return 0
             })
+            return action.payload==='up'?newState:newState.reverse()
         }
 
 
         case 'check': {
-            // filter() создаёт новый массив? или нужно в ручную?...
             return state.filter(el=> el.age >= action.payload) // need to fix
         }
         default:
